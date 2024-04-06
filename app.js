0e329a943b629761e28b62347bb71988e5e72788
Rahul-Sharma2003 <143937371+Rahul-Sharma2003@users.noreply.github.com>
2024-04-06 19:02:38 +0530

    created React app by myself

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading=React.createElement("h1",{
     id:"headding",
     xyz:"abc"
@@ -24,7 +26,7 @@ const parent=React.createElement(
     {id:"parent"},
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"I,m the h1"),
-        React.createElement("h2",{},"I,m the h2")
+        React.createElement("h2",{},"I,m the h2....")
     ])
 )
 
