01836d2af34fb332312dedd3333b4e38e25392e3
Rahul-Sharma2003 <143937371+Rahul-Sharma2003@users.noreply.github.com>
2024-04-06 16:03:05 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,31 @@
+const heading=React.createElement("h1",{
+    id:"headding",
+    xyz:"abc"
+},"Hello World from React!!"); // {} it is for giving the attribute to the object. like id:"heading"
+// heading is an object not the h1 yet
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+/*
+let we want to something like this
+<div id="parent">
+    <div id="child">
+    <h1>I am h1 tag</h1>
+    <h2>I am h2 tag </h2>
+    </div>
+</div>
+
+if you want to make more than one child then you have to create in an array.
+other wise just give the normal.
+
+*/ 
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I,m the h1"),
+        React.createElement("h2",{},"I,m the h2")
+    ])
+)
+
+root.render(parent);// root.render converting the object into h1 and rendering it into root.
